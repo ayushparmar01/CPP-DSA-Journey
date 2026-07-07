3478a35a9e87d1287bee170dd77575b5a7d6de90
ayushparmar01 <atushparmar421@gmail.com>
2026-07-07 22:21:53 +0530

    Added lecture 42 files

---
+++ LECTURE-42/142oops.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Hero {
+
+    // properties
+    // when class is empty than its size of 1 byte
+
+    
+    
+};
+
+int main() {
+
+    // creation of an object
+    Hero h1;
+
+    cout << "size: " << sizeof(h1) << endl;
+}
